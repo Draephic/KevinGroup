d0a6a8f50aa04c1f16fa594b27e3d7a071b109d9
kevin <kevinalexandercastro@gmail.com>
2016-06-21 17:48:44 -0400

    LE CAMBIE EL TITULO EN INDEX.CSHTML

---
+++ EjemploGit/EjemploGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EjemploGit.Startup))]
+namespace EjemploGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
